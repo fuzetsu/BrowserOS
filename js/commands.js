357cb11786ee548ae8bcacb56d8f57f5531f3c28
fuzetsu <fuzetsu490@gmail.com>
2013-07-23 16:17:08 -0400

    removed testing log of numbers in avg command

--- js/commands.js
+++ js/commands.js
@@ -141,7 +141,6 @@ system.createCommands = function($scope, fileSystem) { // TODO - look into remov
         avg: function() {
             if(!arguments[0]) return "usage: avg [numbers to average]";
             return _.reduce(arguments, function(lastNumber, number) {
-                console.log(lastNumber);
                 return lastNumber + parseFloat(number, 10);
             }, 0) / arguments.length || "error: input must be valid numbers.";
         }
